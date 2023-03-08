8ac7565a80019b7c6e6897167720f51fd19bafe9
Ranjit Mane <ranjitmane48@gmail.com>
2023-03-08 09:10:14 +0530

    [[Restful-web-service-with-node-and-express]]: added basic setup and packages

--- Projects/src/Pluralsight/Restful_Web_Services_With_Nodejs_And_Express/app.js
+++ Projects/src/Pluralsight/Restful_Web_Services_With_Nodejs_And_Express/app.js
@@ -1,12 +1,16 @@
-let express = require('express');
+const express = require('express');
 
 const app = express();
-let port = process.env.PORT | 5000;
 
-app.get('/', (req,res) => {
-    res.end("Welcome to my API!!");
-})
+// eslint-disable-next-line no-undef
+const port = process.env.PORT | 5000;
 
+app.get('/', (req, res) => {
+  res.end(`Welcome to my API!! \nToday's date is ${new Date().toLocaleDateString()}`);
+});
+
+// eslint-disable-next-line linebreak-style
 app.listen(port, () => {
-    console.log("Server is listening on port " + port);
-})
+  // eslint-disable-next-line no-console
+  console.log(`Server is listening on port: ${port} at ${new Date().toLocaleDateString()}`);
+});
