d28823bc090922d6ec737b3d1208af3e59e342df
Ranjit Mane <ranjitmane48@gmail.com>
2023-03-16 00:37:57 +0530

    folder restructured and paths are corrected

--- Projects/src/Udemy/express-js-app-1/server/body-parser-server.js
+++ Projects/src/Udemy/express-js-app-1/server/body-parser-server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('../routes/admin');
+const shopRoutes = require('../routes/shop');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 
-app.use("/", (req,res) => {
+app.get("/", (req,res) => {
     res.send("Hello from Express!");
 });
 
 app.use((req,res,next) => {
-    res.status(404).send('Page Not Found!!');
+    console.log("error callback invoked");
+    res.status(404).sendFile(path.join(__dirname, '../', 'views' , '404.html'));
 });
 
 app.listen(3000, ()=> {
